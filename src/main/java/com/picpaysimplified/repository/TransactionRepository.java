0f8d6301ce2e1fe1ace51f2e0fb4e778506cd5ed
DKFAEL <rafaelnunesemp@hotmail.com>
2023-08-15 23:41:28 -0300

    Create TransactionRepository

---
+++ src/main/java/com/picpaysimplified/repository/TransactionRepository.java
@@ -0,0 +1,7 @@
+package com.picpaysimplified.repository;
+
+import com.picpaysimplified.domain.user.transaction.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransactionRepository  extends JpaRepository<Transaction, Long> {
+}
